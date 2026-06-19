25672ad3bf6ebe3b97d60d9fcc99d5711bde752f
ErdniSamkhaev <samhaev.e@yandex.ru>
2026-06-19 15:53:23 +0300

    циклы

--- hello/main.go
+++ hello/main.go
@@ -2,59 +2,109 @@ package main
 
 import "fmt"
 
-//	func main() {
-//		var age int = 32
-//		var name string = "Erdni"
-//		fmt.Println(name, age)
-//	}
-//
-// ------------------------------------------------------------
-//
-//	func main() {
-//		var city = "Riga"   // Тип выведен сам string
-//		// := работает только внутри функций
-//		country := "Latvia" // Короткое объявление, тоже выводит тип
-//		fmt.Println(city, country)
-//	}
-//
-// --------------------------------------------------------------
+// func main() {
+// 	age := 20
+// 	// скобок в if как в JS нет
+// 	if age >= 18
+// 		fmt.Println("совершенолетний")
+// 	else {
+// 		fmt.Println("нет")
+// 	}
+// }
+
+// --------------------------------
 //
 //	func main() {
-//		// zero value
-//		//  Выведет 0 0  false
-//		// В Go у каждого типа есть нулевое значение: число → 0,
-//		// строка → "" (пустая, поэтому между 0 и false ты видишь двойной пробел — там пустая строка), bool → false.
-//		//  Неинициализированных «мусорных» переменных в Go не бывает.
-//		var count int
-//		var price float64
-//		var label string
-//		var ready bool
-//		fmt.Println(count, price, label, ready)
+//		age := 20
+//		// Переменная doubled создается внутри условия if
+//		if doubled := age * 2; doubled > 30 {
+//			fmt.Println(doubled, "больше 30")
+//		}
 //	}
 //
-// ---------------------------------------------------
+// --------------------------------------
+// тернарки как в JS неь
+// status := age > 18 ? "взрослый" : "ребенок"
+// в Go так задумано: меньше способов написать одно и то же = меньше холиваров и проще читать чужой код
+// func main() {
+// 	age := 20
+// 	var status string
+// 	if age > 18 {
+// 		status = "взрослый"
+// 	} else {
+// 		status = "ребенок"
+// 	}
+// 	fmt.Println(status)
+// }
+
+// while нет , только for
 //
-//	func main () {
-//		var a int = 5
-//		var b float64 = 2.5
-//		// fmt.Println(a + b) так низя складывать как в JS
-//		fmt.Println(float64(a) + b)
+//	func main() {
+//		for i := 0; i < 3; i++ {
+//			fmt.Println(i)
+//		}
 //	}
 //
-// ---------------------------------------------------
+// ------------------------------------------
+// в Go нет while и do-while. Нужен цикл «пока условие истинно» — это тот же for, просто с одним условием:
 //
-//	func main () {
-//		const pi = 3.14159
-//		const greeting = "Привет"
-//		fmt.Println(pi, greeting)
-//		// константу низя менять
-//		pi = 4
-//		fmt.Println(pi)
+//	func main() {
+//		n := 0
+//		for n < 3 {
+//			fmt.Println(n)
+//			n++
+//		}
 //	}
 //
-// ----------------------------------------------
+// ------------------------------------------------
+// бесконечный for + break.
+//
+//	func main() {
+//		count := 0
+//		for {
+//			if count == 3 {
+//				break
+//			}
+//			fmt.Println(count)
+//			count++
+//		}
+//	}
+//
+// ------------------------------------------------
+// тут нет break после каждого case
+// В Go switch по умолчанию не проваливается в следующий case — отработал свой и вышел сам.
+//
+//	func main() {
+//		day := 3
+//		switch day {
+//		case 1:
+//			fmt.Println("ПОнедельгтк")
+//		case 3:
+//			fmt.Println("Среда")
+//		default:
+//			fmt.Println("Другой")
+//		}
+//	}
+//
+// Можно несколько значений в одном case
+//
+//	func main() {
+//		day := 3
+//		switch day {
+//		case 6, 7:
+//			fmt.Println("выходной")
+//		default:
+//			fmt.Println("будний")
+//		}
+//	}
+//
+// --------------------------------
 func main() {
-	var weight float64 = 78
-	var reps int = 12
-	fmt.Println(weight + (float64(reps)))
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("Четное:", i)
+		} else {
+			fmt.Println("Нечетное:", i)
+		}
+	}
 }
