6c75a00a1eab25dd31d54ef8045ea905af2ce881
ErdniSamkhaev <samhaev.e@yandex.ru>
2026-06-20 16:33:14 +0300

    начало интерфейсов

--- hello/main.go
+++ hello/main.go
@@ -2,146 +2,152 @@ package main
 
 import "fmt"
 
-// Структуры
-// Поле Hobbies — это срез внутри структуры.
-// К нему работает append
-// type User struct {
-// 	Name    string
-// 	Hobbies []string
-// }
+// Просто два типа
+// type Dog struct{ Name string }
+// type Cat struct{ Name string }
+
+// func (d Dog) Sound() string { return "Гав" }
+// func (c Cat) Sound() string { return "Мяу" }
 
 //	func main() {
-//		u := User{Name: "Erdni", Hobbies: []string{"boxing", "go"}}
-//		u.Hobbies = append(u.Hobbies, "dogs")
-//		// для отладки "%+v\n"
-//		fmt.Printf("%+v\n", u)
-//	}
-//
-// -------------------------
-// Структура внутри структуры
-// type Address struct {
-// 	City    string
-// 	Country string
-// }
-// // Address — отдельный тип, и он поле внутри User
-// type User struct {
-// 	Name    string
-// 	Address Address
-// }
-
-//	func main() {
-//		u := User{
-//			Name:    "Erdni",
-//			Address: Address{City: "Riga", Country: "Latvia"},
-//		}
-//		fmt.Println(u.Address.City)
-//	}
-//
-// ------------------------------------
-// Структура с картой внутри
-// но здесь проблема
-// Я создал Account, но не задал поле Balances.
-// Значит Balances встал в своё zero value.
-//  А zero value карты — это nil
-// в nil-срез append делать можно
-// а в nil-карту писать — паника.
-// Карта требует, чтобы её сначала создали, срез — нет.
-// type Account struct {
-// 	Owner    string
-// 	Balances map[string]int
-// }
-
-//	func main() {
-//		a := Account{Owner: "Erdni"}
-//		a.Balances["USD"] = 100
-//		fmt.Println(a.Balances)
+//		d := Dog{Name: "Курама"}
+//		c := Cat{Name: "Мося"}
+//		fmt.Println(d.Sound())
+//		fmt.Println(c.Sound())
 //	}
 //
 // -------------------
-// Создаем нормально с инициализированной картой
-// type Account struct {
-// 	Owner    string
-// 	Balances map[string]int
+// а если я хочу функцию, которая примет любого, кто умеет издавать звук — и собаку, и кошку, и кого угодно ещё
+// добавляем интерфейс
+// type Dog struct{ Name string }
+// type Cat struct{ Name string }
+
+// func (d Dog) Sound() string { return "Гав" }
+// func (c Cat) Sound() string { return "Мяу" }
+
+// // интерфейс
+// type Sounder interface {
+// 	Sound() string
 // }
 
-// func NewAccount(owner string) Account {
-// 	return Account{
-// 		Owner:    owner,
-// 		Balances: map[string]int{}, //создаем пустую карту, не nil
-// 	}
-// }
-
-//	func main() {
-//		a := NewAccount("Erdni")
-//		a.Balances["USD"] = 100 //теперь не паникует
-//		fmt.Println(a.Balances)
-//	}
-//
-// ----------------------------
-// конструктор, возвращающий указатель
-// type User struct {
-// 	Name string
-// 	Age  int
-// }
-
-// func NewUser(name string) *User {
-// 	// &User{...} — создаёт структуру и сразу даёт её адрес. Тип возврата — *User
-// 	return &User{Name: name}
+// // функция принимающая интерефейс
+// func describe(s Sounder) {
+// 	fmt.Println("Звук:", s.Sound())
 // }
 
 //	func main() {
-//		u := NewUser("Erdni")
-//		fmt.Println(u.Name)
+//		d := Dog{Name: "Курама"}
+//		c := Cat{Name: "Мося"}
+//		describe(d)
+//		describe(c)
 //	}
 //
-// ----------------------
-// конструктор с проверкой.
-// type User struct {
-// 	Name string
-// 	Age  int
+// -------------------------------
+// Делаем все тоже самое но с добавлением типа без метода
+// и будет ошибка implement Sounder
+// type Dog struct{ Name string }
+// type Cat struct{ Name string }
+// type Rock struct{}
+
+// func (d Dog) Sound() string { return "Гав" }
+// func (c Cat) Sound() string { return "Мяу" }
+
+// // интерфейс
+// type Sounder interface {
+// 	Sound() string
 // }
-// // Возврат (*User, error) — указатель И ошибка, тот же мультивозврат, что у strconv.Atoi
-// func NewUser(name string, age int) (*User, error) {
-// 	if age < 0 {
-// 		// Плохой ввод → nil (zero value указателя) + заполненная ошибка. Хороший → адрес + nil
-// 		return nil, fmt.Errorf("Возврат не может быть отрицательным: %d", age)
-// 	}
-// 	return &User{Name: name, Age: age}, nil
+
+// // функция принимающая интерефейс
+// func describe(s Sounder) {
+// 	fmt.Println("Звук:", s.Sound())
 // }
 
 //	func main() {
-//		u, err := NewUser("Erdni", -5)
-//		if err != nil {
-//			fmt.Println("ошибка", err)
-//			return
-//		}
-//		fmt.Println(u.Name, u.Age)
+//		d := Dog{Name: "Курама"}
+//		c := Cat{Name: "Мося"}
+//		r := Rock{}
+//		describe(d)
+//		describe(c)
+//		describe(r)
 //	}
 //
-// ----------------------
+// -----------------------------
+// Теперь добавляем третий тип так, чтобы не трогать describe(функицю принимающая интерфейс)
+// type Dog struct{ Name string }
+// type Cat struct{ Name string }
+// type Bird struct{ Name string }
+
+// func (d Dog) Sound() string  { return "Гав" }
+// func (c Cat) Sound() string  { return "Мяу" }
+// func (b Bird) Sound() string { return "Кря" }
+
+// // интерфейс
+// type Sounder interface {
+// 	Sound() string
+// }
+
+// // функция принимающая интерефейс
+// func describe(s Sounder) {
+// 	fmt.Println("Звук:", s.Sound())
+// }
+
+// func main() {
+// 	d := Dog{Name: "Курама"}
+// 	c := Cat{Name: "Мося"}
+// 	describe(d)
+// 	describe(c)
+// 	describe(Bird{Name: "Кеша"})
+// }
+// ---------------------------------------
+// теперь error
+// type Dog struct{ Name string }
+// type Cat struct{ Name string }
+// type Bird struct{ Name string }
+
+// func (d Dog) Sound() string  { return "Гав" }
+// func (c Cat) Sound() string  { return "Мяу" }
+// func (b Bird) Sound() string { return "Кря" }
+
+// // интерфейс
+// type Sounder interface {
+// 	Sound() string
+// }
+// // интрефейс ошибки
+// type error interface{
+// 	Error() string
+// }
+
+// // функция принимающая интерефейс
+// func describe(s Sounder) {
+// 	fmt.Println("Звук:", s.Sound())
+// }
+
+//	func main() {
+//		d := Dog{Name: "Курама"}
+//		c := Cat{Name: "Мося"}
+//		describe(d)
+//		describe(c)
+//		describe(Bird{Name: "Кеша"})
+//	}
+//
+// -------------------------------
 // Задача
-type Wallet struct {
-	Owner string
-	// Карта — это «коробка пар ключ→значение
-	Coins map[string]int
+// Создаем типы простые
+type English struct{}
+type Russian struct{}
+// даем типам метод Greet
+func (e English) Greet() string { return "Hello" }
+func (r Russian) Greet() string { return "Привет" }
+// интерфейс
+type Greeter interface {
+	Greet() string
 }
-
-// Инициализируем карту, иначе она nil, а запись в nil-карту = паника
-func NewWallet(owner string) *Wallet {
-	return &Wallet{
-		Owner: owner,
-		Coins: map[string]int{},
-	}
-}
-
-// метод привязанный к Wallet
-func (w *Wallet) Add(coin string, amount int) {
-	w.Coins[coin] += amount
+// функция принимающая интерфейс
+func sayHi(g Greeter) {
+	fmt.Println(g.Greet())
 }
 
 func main() {
-	w := NewWallet("Erdni")
-	w.Add("BTC", 1)
-	w.Add("BTC", 2)
-	fmt.Printf("%+v\n", w)
+	sayHi(English{})
+	sayHi(Russian{})
 }
