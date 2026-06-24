bc8f2fd39908d360f876535d1bc0836f507c5622
ErdniSamkhaev <samhaev.e@yandex.ru>
2026-06-25 00:57:29 +0300

    пока что проверка без горутин и каналов

--- hello/main.go
+++ hello/main.go
@@ -5,16 +5,36 @@ import (
 	"net"
 	"time"
 )
+// тип для ответа
+type Result struct {
+	Address  string
+	Alive    bool
+	Duration time.Duration
+}
+
+func check(address string) Result {
+	start := time.Now()
+	// для проверки соединения
+	conn, err := net.DialTimeout("tcp", address, 2*time.Second)
+	elapsed := time.Since(start)
+
+	if err != nil {
+		return Result{Address: address, Alive: false, Duration: elapsed}
+	}
+	defer conn.Close()
+	return Result{Address: address, Alive: true, Duration: elapsed}
+}
 
 func main() {
-	address := "google.com:80"
-
-	conn, err := net.DialTimeout("tcp", address, 2*time.Second)
-	if err != nil {
-		fmt.Println("Хост не доступен, ошибка:", err)
-		return
+	addresses := []string{
+		"google.com:443",
+		"github.com:443",
+		"192.0.2.1:80", // мертвый
+		"google.com:9999", //закрытый
 	}
-	// сразу закрываем
-	defer conn.Close()
-	fmt.Println(address, "доступен")
+
+	for _, addr := range addresses {
+		result := check(addr)
+		fmt.Printf("%s | alive=%v | %v\n", result.Address, result.Alive, result.Duration)
+	}
 }
