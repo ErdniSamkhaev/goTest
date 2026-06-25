5d877e91705db80df97b2ba199f9894924805628
ErdniSamkhaev <samhaev.e@yandex.ru>
2026-06-25 21:26:35 +0300

    сырой вариант чекера хостов

--- hello/main.go
+++ hello/main.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"time"
 )
+
 // тип для ответа
 type Result struct {
 	Address  string
@@ -12,29 +13,38 @@ type Result struct {
 	Duration time.Duration
 }
 
-func check(address string) Result {
+func check(address string, ch chan Result) {
 	start := time.Now()
 	// для проверки соединения
 	conn, err := net.DialTimeout("tcp", address, 2*time.Second)
 	elapsed := time.Since(start)
 
 	if err != nil {
-		return Result{Address: address, Alive: false, Duration: elapsed}
+		ch <- Result{Address: address, Alive: false, Duration: elapsed}
+		return
 	}
 	defer conn.Close()
-	return Result{Address: address, Alive: true, Duration: elapsed}
+	ch <- Result{Address: address, Alive: true, Duration: elapsed}
 }
 
 func main() {
+	start := time.Now()
 	addresses := []string{
 		"google.com:443",
 		"github.com:443",
-		"192.0.2.1:80", // мертвый
+		"192.0.2.1:80",    // мертвый
 		"google.com:9999", //закрытый
 	}
 
+	ch := make(chan Result)
+	// Запускаем все проверки паралельно
 	for _, addr := range addresses {
-		result := check(addr)
+		go check(addr, ch)
+	}
+
+	for i := 0; i < len(addresses); i++ {
+		result := <-ch
 		fmt.Printf("%s | alive=%v | %v\n", result.Address, result.Alive, result.Duration)
 	}
+	fmt.Println("Всего заняло:", time.Since(start))
 }
