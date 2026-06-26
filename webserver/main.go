c1b107799a564f11f8aa250c2de03674db139c1f
ErdniSamkhaev <samhaev.e@yandex.ru>
2026-06-26 11:40:20 +0300

    json

--- webserver/main.go
+++ webserver/main.go
@@ -1,57 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
+	// "strconv"
 )
+
+type Status struct {
+	Service string `json:"service"`
+	Version string `json:"version"`
+	Healthy bool   `json:"healthy"`
+}
+
+type Result struct {
+	Address string `json:"address"`
+	Alive   bool   `json:"alive"`
+}
+
+func results(w http.ResponseWriter, r *http.Request) {
+	data := []Result{
+		{Address: "google.com:443", Alive: true},
+		{Address: "github.com:443", Alive: true},
+		{Address: "192.0.2.1:80", Alive: false},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	s := Status{
+		Service: "checker-api",
+		Version: "1.0",
+		Healthy: true,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(s)
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Ты запросил путь: %s\n", r.URL.Path)
 }
 
-func about(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "О сервере: чекер проект, версия 1.0")
-}
-
-func ping(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Метод запроса: %s\n", r.Method)
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "незнакомец"
-	}
-	fmt.Fprintf(w, "Привет, %s\n", name)
-}
-
-func sum(w http.ResponseWriter, r *http.Request) {
-	aStr := r.URL.Query().Get("a")
-	bStr := r.URL.Query().Get("b")
-
-	a, err := strconv.Atoi(aStr)
-	if err != nil {
-		fmt.Fprintf(w, "Ошибка: '%s' не число\n", aStr)
+func addTarget(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "только POST", http.StatusMethodNotAllowed)
 		return
 	}
 
-	b, err := strconv.Atoi(bStr)
+	var target Result
+	err := json.NewDecoder(r.Body).Decode(&target)
 	if err != nil {
-		fmt.Fprintf(w, "Ошибка: '%s' не число\n", bStr)
+		http.Error(w, "Плохой JSON", http.StatusBadRequest)
 		return
 	}
 
-	fmt.Fprintf(w, "Сумма: %d\n", a+b)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println(w, "Принял хост: %s (alive=%v)\n", target.Address, target.Alive)
 }
 
 func main() {
 	http.HandleFunc("/", home)
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/ping", ping)
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/sum", sum)
+	http.HandleFunc("/status", status)
+	http.HandleFunc("/results", results)
+	http.HandleFunc("/addTarget", addTarget)
 
-	fmt.Println("Сервер на http://localhost:8080")
+	fmt.Println("Сервер запущен на: http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
